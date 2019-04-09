9d1d2524ea73105df1ee4429d7bd37268514914a
Oldmo <35811214+105306006@users.noreply.github.com>
2019-04-09 15:50:10 +0800

    oldmo

--- server/src/index.js
+++ server/src/index.js
@@ -3,15 +3,17 @@ require('./db/mongoose')
 const User = require('./models/user')
 const bodyParser = require('body-parser');
 const authenticate = require('./middleware/authenticate')
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(bodyParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/login', authenticate ,async (req,res) => {
-    
+
 })
 
 app.listen(port,() => {
